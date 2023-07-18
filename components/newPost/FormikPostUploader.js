7f7aab4e72454d074748f7b1b5f16f7d3f1d0b61
SoumyadiptoPal <soumyadiptocodes@gmail.com>
2023-07-18 22:10:51 +0530

    Added doc picker

--- components/newPost/FormikPostUploader.js
+++ components/newPost/FormikPostUploader.js
@@ -14,64 +14,85 @@ const PlaceholderImg =
 
 const FormikPostUploader = ({navigation}) => {
   const [thumbnailUrl, setThumbnailUrl] = useState(PlaceholderImg);
+  const [caption,setCaption]=useState("");
+  const handleSubmit=()=>{
 
-  return (
-    <Formik
-      initialValues={{ caption: "", imageUrl: "" }}
-      onSubmit={(values) => {
-        console.log(values)
-        navigation.goBack()}}
-      validationSchema={uploadPostSchema}
-      validateOnMount={true}
-    >
-      {({
-        handleBlur,
-        handleChange,
-        handleSubmit,
-        values,
-        errors,
-        isValid,
-      }) => (
-        <>
-          <View style={{margin:20, justifyContent:"space-between",flexDirection:"row"}}>
-            <Image
-              source={{ uri: thumbnailUrl ? thumbnailUrl:PlaceholderImg }}
-              style={{ width: 100, height: 100 }}
-            />
-            <View style={{flex:1, marginLeft:10}}>
-            <TextInput
-            placeholder="Enter Captions...."
-            placeholderTextColor="gray"
-            multiline={true}
-            style={{ color: "white", fontSize: 20 }}
-            onChangeText={handleChange("caption")}
-            onBlur={handleBlur('caption')}
-            value={values.caption} 
-          />      
-        </View>
-          </View>
-          <Divider width={0.2} orientation="vertical"/>
+  }
+  // return (
+  //   <Formik
+  //     initialValues={{ caption: "", imageUrl: "" }}
+  //     onSubmit={(values) => {
+  //       console.log(values)
+  //       navigation.goBack()}}
+  //     validationSchema={uploadPostSchema}
+  //     validateOnMount={true}
+  //   >
+  //     {({
+  //       handleBlur,
+  //       handleChange,
+  //       handleSubmit,
+  //       values,
+  //       errors,
+  //       isValid,
+  //     }) => (
+  //       <>
+  //         <View style={{margin:20, justifyContent:"space-between",flexDirection:"row"}}>
+  //           <Image
+  //             source={{ uri: thumbnailUrl ? thumbnailUrl:PlaceholderImg }}
+  //             style={{ width: 100, height: 100 }}
+  //           />
+  //           <View style={{flex:1, marginLeft:10}}>
+  //           <TextInput
+  //           placeholder="Enter Captions...."
+  //           placeholderTextColor="gray"
+  //           multiline={true}
+  //           style={{ color: "white", fontSize: 20 }}
+  //           onChangeText={handleChange("caption")}
+  //           onBlur={handleBlur('caption')}
+  //           value={values.caption} 
+  //         />      
+  //       </View>
+  //         </View>
+  //         <Divider width={0.2} orientation="vertical"/>
 
-          <TextInput
-          onChange={e=>setThumbnailUrl(e.nativeEvent.text)}
-            placeholder="Enter Image Url..."
+  //         <TextInput
+  //         onChange={e=>setThumbnailUrl(e.nativeEvent.text)}
+  //           placeholder="Enter Image Url..."
+  //           placeholderTextColor="gray"
+  //           style={{ color: "white", fontSize:18 }}
+  //           onChangeText={handleChange('imageUrl')}
+  //           onBlur={handleBlur('imageUrl')}
+  //           value={values.imageUrl}
+  //         />
+  //         {errors.imageUrl && (
+  //           <Text style={{fontSize:10, color:"red"}}>
+  //             {errors.imageUrl}
+  //           </Text>
+  //         )}
+
+  //         <Button onPress={handleSubmit} title="Share" disabled={!isValid}>Share</Button>
+  //       </>
+  //     )}
+  //   </Formik>
+  // );
+  return(
+    <View>
+      <View>
+        {/* <Image source={{ uri: thumbnailUrl ? thumbnailUrl:PlaceholderImg }}
+        style={{ width: 100, height: 100 }}
+         /> */}
+      </View>
+      <View>
+      <TextInput
+          onChange={e=>setCaption(e.nativeEvent.text)}
+            placeholder="Enter Captions..."
             placeholderTextColor="gray"
-            style={{ color: "white", fontSize:18 }}
-            onChangeText={handleChange('imageUrl')}
-            onBlur={handleBlur('imageUrl')}
-            value={values.imageUrl}
+            style={{ color: "white", fontSize:18 }}  
           />
-          {errors.imageUrl && (
-            <Text style={{fontSize:10, color:"red"}}>
-              {errors.imageUrl}
-            </Text>
-          )}
-
-          <Button onPress={handleSubmit} title="Share" disabled={!isValid}>Share</Button>
-        </>
-      )}
-    </Formik>
-  );
+      </View>
+      <Button onPress={handleSubmit} title="Share">Share</Button>
+    </View>
+  )
 };
 
 export default FormikPostUploader;
